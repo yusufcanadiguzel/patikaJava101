b275b3fde66f95b047f6f4bb09c3506ca0369ce1
Yusufcan Adıgüzel <yusufcanadgzl@outlook.com>
2024-03-27 23:49:56 +0300

    Nearest and Furthest Example
    En küçük ve en büyük elemana en yakın sayıyıları bulan program.

--- src/ArrayHelper.java
+++ src/ArrayHelper.java
@@ -1,4 +1,7 @@
+import java.util.Arrays;
+
 public class ArrayHelper {
+    //Verilen dizideki elemanların harmonik ortalmasını hesaplayan program.
     static void harmonicSeries(int[] numbers){
         double result = 0;
 
@@ -7,5 +10,20 @@ public class ArrayHelper {
         }
 
         System.out.println("Dizinin harmonik serisi: " + result);
+    }
+
+    //En küçük ve en büyük elemana en yakın sayıyıları bulan program.
+    static void nearestAndFurthest(int[] numbers){
+        int min, max, nearest, furthest;
+
+        Arrays.sort(numbers);
+
+        min = numbers[0];
+        max = numbers[numbers.length - 1];
+        nearest = numbers[Arrays.binarySearch(numbers, min) + 1];
+        furthest = numbers[Arrays.binarySearch(numbers, max) - 1];
+
+        System.out.println("En küçük sayıya en yakın sayı: " + nearest);
+        System.out.println("En büyük sayıya en yakın sayı: " + furthest);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         int[] numbers = {1, 2, 3, 4, 5};
 
-        ArrayHelper.harmonicSeries(numbers);
+        ArrayHelper.nearestAndFurthest(numbers);
     }
 }
