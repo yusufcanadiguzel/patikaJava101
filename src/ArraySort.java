ae712082cc8238a95f9726c9ff7e9b58f04d40b2
Yusufcan Adıgüzel <yusufcanadgzl@outlook.com>
2024-03-28 22:53:50 +0300

    Array Sort Example
    Kullanıcıdan dizinin eleman sayısını ve elemanları alıp küçükten büyüğe doğru ekrana yazdıran program.

---
+++ src/ArraySort.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArraySort {
+    static void arraySort(){
+        int[] numbers;
+        int arrayLenght;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Dizinin boyutu n : ");
+        arrayLenght = scanner.nextInt();
+        numbers = new int[arrayLenght];
+
+        System.out.println("\nDizinin elemanlarını giriniz : ");
+        for (int i = 0; i < arrayLenght; i++){
+            System.out.print(i + 1 + ". Elemanı : ");
+            numbers[i] = scanner.nextInt();
+            System.out.println();
+        }
+
+        Arrays.sort(numbers);
+
+        System.out.print("Sıralama : ");
+        for (int number : numbers){
+            System.out.print(number + " ");
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = {2, 7, 2, 2, 3, 4, 10, 21, 1, 4, 9, 1};
-        ArrayHelper.isRepeatedEvenNumbers(numbers);
+        ArraySort.arraySort();
     }
 }
