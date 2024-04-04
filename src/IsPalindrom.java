1da191f6f1f747bf1887ce8f1b41e0efd0795775
Yusufcan Adıgüzel <yusufcanadgzl@outlook.com>
2024-04-04 05:27:15 +0300

    Palindrom Word Example
    Girilen metnin palindrom olup olmadığını bulan algoritma.

--- src/IsPalindrom.java
+++ src/IsPalindrom.java
@@ -1,5 +1,5 @@
 public class IsPalindrom {
-    static boolean isPalindrom(int number){
+    static boolean isPalindromNumber(int number){
         int tempNumber = number, total = 0;
 
         while(tempNumber != 0){
@@ -8,5 +8,18 @@ public class IsPalindrom {
         }
 
         return total == number;
+    }
+
+    static void isPalindromWord(String word){
+        String tempWord = "";
+        for(int i = word.length() - 1; i >= 0; i--){
+            tempWord += word.charAt(i);
+        }
+
+        if(word.equals(tempWord)){
+            System.out.println("Girdiğiniz kelime palindromdur.");
+        }else{
+            System.out.println("Girdiğiniz kelime palindrom değildir.");
+        }
     }
 }
